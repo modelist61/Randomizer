fd1a98dc83c2c55e8336565ae50dc23f699a9a71
Dmitry Tokarev <modelist61@bk.ru>
2021-02-10 22:49:21 +0300

    Initial Commit

---
+++ PoniRunning/PoniRunningApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoniRunningApp.swift
+//  PoniRunning
+//
+//  Created by Dmitry Tokarev on 10.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct PoniRunningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
